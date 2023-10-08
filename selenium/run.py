3adace5d7144feb298412295a1f9274b4e9e3c57
devboss0924 <smallstar0924@gmail.com>
2023-10-08 23:44:29 +0900

    added pupetter folder

--- selenium/run.py
+++ selenium/run.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-import time
 import re
 import undetected_chromedriver as uc
 from webdriver_manager.chrome import ChromeDriverManager
@@ -15,30 +14,31 @@ import os
 from seleniumbase import Driver
 import time
 import csv
+from datetime import datetime, timedelta
 
 parser = argparse.ArgumentParser()
 parser.add_argument('message', help='A message to display')
 args = parser.parse_args()
-message = args.message
-print("argment>>> ", message)
+start_date = args.message
+print("start_date >>> ", start_date) ################## data to send
+rentYear = start_date.split("/")[2]
+rentMonth = start_date.split("/")[0]
+rentDate= start_date.split("/")[1]
 
+startDate = datetime(int(rentYear), int(rentMonth), int(rentDate))
 
+endDate = startDate + timedelta(days=330)
+# print("result_date >>> ", endDate)
+middle_end_date = str(endDate).split(" ")[0]
+# print("middle end date >>> ", middle_end_date)
+end_date = middle_end_date.split("-")[1] + "/" + middle_end_date.split("-")[2] + "/" + middle_end_date.split("-")[0]
+print("end_date >>> ", end_date) ################## data to send
 
 with open('fullLocation.csv', mode='r') as file:
   reader = csv.reader(file)
   for row in reader:
-    print(row[2])
-
-
-
-
-
-
-
-
-
-
-
+    targetLocation = row[2].split("/")[-1]
+    print(">>>>>>>>>>>>>", targetLocation) ################### data to send
 
 
 
