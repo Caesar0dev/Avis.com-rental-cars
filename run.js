0033842a72d2618d6327a9088a840163ff800698
devboss0924 <smallstar0924@gmail.com>
2023-10-14 10:14:05 +0900

    fixed

--- run.js
+++ run.js
@@ -3,12 +3,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const e = require("express");
 
-const start_date = process.argv[2];
-const givenDate = new Date(start_date);
-const newDate = new Date(givenDate);
-newDate.setDate(givenDate.getDate() + 330);
-const formattedDate = newDate.toISOString().split('T')[0];
-const end_date = formattedDate.split("-")[1] + "/" + formattedDate.split("-")[2] + "/" + formattedDate.split("-")[0];
+let givenDate = process.argv[2];
 
 // Read the text file
 const filePath = 'location origin.csv';
@@ -19,15 +14,36 @@ const text = fs.readFileSync(filePath, 'utf8');
 const lines = text.split('\n');
 
 let searchKey = null;
-for (row in lines) {
-    const searchKeyMiddle = lines[row].split("/");
-    searchKey = searchKeyMiddle[searchKeyMiddle.length - 1].split(",")[0];
-    // console.log(searchKey);
+let start_date = null;
+let end_date = null;
+// let index = 0;
+
+for (let i = 1; i < 31; i++) {
+    // console.log(">>> ", i);
+    // const start_date = new Date(givenDate);
+    const newDate = new Date(givenDate);
+    newDate.setDate(newDate.getDate() + i);
+    const startDate = newDate.toISOString().split('T')[0];
+    start_date = startDate.split("-")[1] + "/" + startDate.split("-")[2] + "/" + startDate.split("-")[0];
+    newDate.setDate(newDate.getDate() + 330);
+    const endDate = newDate.toISOString().split('T')[0];
+    end_date = endDate.split("-")[1] + "/" + endDate.split("-")[2] + "/" + endDate.split("-")[0];
+    
+    for (row in lines) {
+        const searchKeyMiddle = lines[row].split("/");
+        searchKey = searchKeyMiddle[searchKeyMiddle.length - 1].split(",")[0];
+        // console.log("start date >>> ", start_date);
+        // console.log("end date >>> ", end_date);
+        // console.log("searchKey >>> ", searchKey);
+        
+        // index = index + 1;
+        // console.log("------------", index, "---------------");
+
+        ///////////////////////////// put here the scraping code
+
+        /////////////////////////////
+    }
 }
-
-console.log("start date >>> ", start_date);
-console.log("end date >>> ", end_date);
-
 ///////////////////// preparation end ////////////////////////
 
 async function launchBrowser(name, value) {
@@ -58,18 +74,18 @@ async function launchBrowser(name, value) {
 
             // It's an AJAX request
             if (request.url().includes('https://www.avis.com/webapi/reservation/vehicles')) {
-                console.log("request >>> ", request.headers())
+                // console.log("request >>> ", request.headers())
                 
                 BasicPayload = request.postData();
-                console.log("Payload >>> ", BasicPayload);
+                // console.log("Payload >>> ", BasicPayload);
                 
                 if(BasicPayload) {
                     Digital_Token = request.headers()['digital-token'];
-                    console.log("Digital Token >>> ", Digital_Token);
+                    // console.log("Digital Token >>> ", Digital_Token);
                     Cookie = request.headers().cookie;
                     // console.log("Cookie >>> ", Cookie);
                     RecaptchaResponse = request.headers()['g-recaptcha-response'];
-                    console.log("Recaptcha Response >>> ", RecaptchaResponse);
+                    // console.log("Recaptcha Response >>> ", RecaptchaResponse);
 
                     const payloadElements = BasicPayload.split(",");
                     for (index in payloadElements) {
@@ -89,7 +105,7 @@ async function launchBrowser(name, value) {
                         NewPayload = NewPayload + payloadElements[index];
                     }
                     
-                    console.log("NewPayload >>> ", NewPayload);
+                    // console.log("NewPayload >>> ", NewPayload);
                 }
             }
         }
@@ -101,52 +117,104 @@ async function launchBrowser(name, value) {
         timeout: 300000
     });
 
+    // Delay function
+    function delay(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    
+    // Delay for 10 seconds
+    await delay(3000); // 10,000 milliseconds = 10 seconds
+    console.log('step 1')
+    // /html/body/div[4]/section/div[2]/div[1]/span
+    let modalXPath = '/html/body/div[4]/section/div[2]/div[1]/span';
+    try {
+        const [modalClose] = await page.$x(modalXPath);
+        await modalClose.click({timeout:300000});
+    } catch (error) {
+        console.log(error);
+    }
+    
+    // Delay for 10 seconds
+    await delay(3000); // 10,000 milliseconds = 10 seconds
+
+    let secModalXPath = '/html/body/div[4]/section/div[1]/div[1]/span';
+    try {
+        const [secModalClose] = await page.$x(secModalXPath);
+        await secModalClose.click({timeout:300000});
+    } catch (error) {
+        console.log(error);
+    }
+
     await page.waitForSelector('#PicLoc_value', {timeout: 300000});
     await page.type('#PicLoc_value', 'anb');
     await page.$eval('#from', (element) => {
         element.value = '11/01/2023';
     });
+    
+    // Delay for 10 seconds
+    await delay(3000); // 10,000 milliseconds = 10 seconds
     const endDateField = await page.waitForSelector('#to', {timeout: 300000});
+    console.log('step 2', endDateField)
     await endDateField.click({timeout: 300000});
+    
+    // Delay for 10 seconds
+    await delay(3000); // 10,000 milliseconds = 10 seconds
+
     const endDateXPath = '/html/body/div[9]/div/div/div/div/div[3]/table/tbody/tr[4]/td[5]/a';
     const [end_day] = await page.$x(endDateXPath);
     await end_day.click({timeout: 300000});
-    console.log(">>> end date clicked successfully !!!");
     
-    
+    // Delay for 10 seconds
+    await delay(3000); // 10,000 milliseconds = 10 seconds
+
     const findButton = await page.waitForSelector('#res-home-select-car', {timeout: 300000});
     await findButton.click({timeout: 300000});
     console.log(">>> submit button clicked successfully !!!");
 
     // Perform actions that trigger AJAX requests
-    const result = await page.evaluate((Digital_Token, Cookie, RecaptchaResponse, NewPayload) => {
-        
-        if (Digital_Token) {
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', 'https://www.avis.com/webapi/reservation/vehicles', true);
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.setRequestHeader('digital-token', Digital_Token);
-            xhr.setRequestHeader('cookie', Cookie);
-            xhr.setRequestHeader('g-recaptcha-response', RecaptchaResponse);
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        console.info('success: ', xhr.responseText);
-                        newResponse = JSON.parse(xhr.responseText);
-                        console.log(">>>>>>>>>>>", newResponse);
-                    } else {
-                        console.error('error: ', xhr.statusText);
+    
+    // Delay for 10 seconds
+    await delay(20000); // 10,000 milliseconds = 10 seconds
+    setTimeout(async () => {
+        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        const result = await page.evaluate((Digital_Token, Cookie, RecaptchaResponse, NewPayload) => {
+            console.log("Digital Token >>> ", Digital_Token);
+            
+            if (Digital_Token) {
+                console.log("---------------------------Digital token okay!!!-----------------------------")
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', 'https://www.avis.com/webapi/reservation/vehicles', true);
+                xhr.setRequestHeader('Content-Type', 'application/json');
+                xhr.setRequestHeader('Accept', 'application/json, text/plain, */*');
+                xhr.setRequestHeader('Action', 'RES_VEHICLESHOP');
+                xhr.setRequestHeader('Bookingtype', 'Digital');
+                xhr.setRequestHeader('Content-Length', 622);
+                xhr.setRequestHeader('Channel', 'car');
+                xhr.setRequestHeader('digital-token', Digital_Token);
+                xhr.setRequestHeader('g-recaptcha-response', RecaptchaResponse);
+                xhr.setRequestHeader('cookie', Cookie);
+                console.log("new request >>> ", xhr);
+                xhr.onreadystatechange = () => {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            console.info('success: ', xhr.responseText);
+                            newResponse = JSON.parse(xhr.responseText);
+                            console.log(">>>>>>>>>>>", newResponse);
+                        } else {
+                            console.error('error: ', xhr.statusText);
+                        }
                     }
-                }
-            };
-            xhr.onerror = () => xhr.statusText;
-            xhr.send(NewPayload);
-        }
+                };
+                xhr.onerror = () => xhr.statusText;
+                xhr.send(NewPayload);
+            }
+    
+        }, Digital_Token, Cookie, RecaptchaResponse, NewPayload)
+    
+        console.log('eval res->', result)
+        // AJAX requests end
 
-    }, Digital_Token, Cookie, RecaptchaResponse, NewPayload)
-
-    console.log('eval res->', result)
-    // AJAX requests end
+    }, 20000);
 
     /////////////////////////////////////////////////////
     
